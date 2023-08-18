8b62de3e00eb205a71bb0c9e40cf1d89c61540b9
Chekos10 <119345370+Chekos10@users.noreply.github.com>
2023-08-18 21:01:58 +0300

    bug_fix_second
    adding basename to brow router

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import { PersistGate } from 'redux-persist/integration/react';
 ReactDOM.createRoot(document.getElementById('root')).render(
 
   <Provider store={store}>
-  <BrowserRouter>
+  <BrowserRouter basename="/goit-react-hw-08-phonebook">
       <PersistGate  persistor={persistor}>
     <App/>
     </PersistGate>
